b8db4e326c71b6ae6f77b91ec8b63299680acfb6
Vinnie Monaco <vinmonaco@gmail.com>
2013-09-04 13:55:09 -0400

    Initial commit

---
+++ agents/rand.py
@@ -0,0 +1,10 @@
+'''
+Created on Sep 5, 2012
+
+@author: vinnie
+
+
+'''
+
+import random
+output = random.choice(['R','P','S'])
---
+++ agents/tit-for-tat.py
@@ -0,0 +1,16 @@
+'''
+Created on Sep 5, 2012
+
+@author: vinnie
+'''
+
+import random
+
+if input == '':
+    output = random.choice(['R','P','S'])
+elif last == '':
+    output = random.choice(['R','P','S'])
+else:
+    output = last
+    
+last = input
